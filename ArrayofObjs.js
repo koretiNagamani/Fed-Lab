6a8194571deabb04b784bfadda4ecdf6a242a3b5
koretiNagamani <141474554+koretiNagamani@users.noreply.github.com>
2023-09-23 12:03:12 +0530

    Add files via upload

---
+++ ArrayofObjs.js
@@ -0,0 +1,34 @@
+import React,{Component} from'react'
+class ArrayOfObjs extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            countriesinfo: [
+                { name: 'India', capital: 'NewDelhi' },
+                { name: 'China', capital: 'Beijing' },
+                { name: 'USA', capital: 'WashingtonDC' },
+                { name: 'Japan', capital: 'Tokyo' },
+            ]
+        }
+    }
+    render(){
+        const{countriesinfo}=this.state;
+        return(
+            <div>
+                <table border='1'>
+                    <tr>
+                        <th>Country name</th>
+                        <th>capital</th>
+                    </tr>
+                    {countriesinfo.length>0 && countriesinfo.map((country)=>{
+                        return <tr key={country.name}>
+                                  <td>{country.name}</td>
+                                  <td>{country.capital}</td>
+                        </tr>
+                    })}
+                </table>
+            </div>
+        )
+    }
+}
+export default ArrayOfObjs;
